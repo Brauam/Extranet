5853753c155c543de5c80abdfffe8b060c6ff286
Brayan Villar <brayanpedrovillarchipana@gmail.com>
2021-10-05 16:44:56 -0500

    tercera subida

--- src/app/layouts/layouts-app.module.ts
+++ src/app/layouts/layouts-app.module.ts
@@ -5,11 +5,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { AppMaterialModule } from '../app-material.module';
 import { MainComponent } from './main/main.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    AppMaterialModule
+    AppMaterialModule,
+    RouterModule
   ],
   declarations: [
     FooterComponent,
