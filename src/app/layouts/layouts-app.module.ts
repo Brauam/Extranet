78215fef70022837f65f6bd9cbee7c24f5c8c5ff
brayan pedro villar chipana <brayanpedrovillarchipana@gmail.com>
2021-11-08 13:58:18 -0500

    subida

--- src/app/layouts/layouts-app.module.ts
+++ src/app/layouts/layouts-app.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { AppMaterialModule } from '../app-material.module';
 import { MainComponent } from './main/main.component';
 import { RouterModule } from '@angular/router';
 import { AppCommonModule } from '../app-common.module';
